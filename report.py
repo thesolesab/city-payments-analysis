08b9bcbf5f391a6e09aa5c117045003fb7706c34
Jar <thesolesab@gmail.com>
2026-02-20 10:29:05 +0300

    Initial commit

---
+++ report.py
@@ -0,0 +1,60 @@
+import sqlite3
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+conn = sqlite3.connect("data/city.db")
+
+query = """
+SELECT r.name,
+       r.district,
+       p.amount
+FROM residents r
+JOIN payments p
+ON r.id = p.resident_id
+"""
+
+df = pd.read_sql_query(query, conn)
+
+# Общий оборот
+total_money = df["amount"].sum()
+
+# Топ жителей
+top_users = (
+    df.groupby("name")["amount"]
+      .sum()
+      .sort_values(ascending=False)
+      .head(3)
+)
+
+# Топ районов
+top_districts = (
+    df.groupby("district")["amount"]
+      .sum()
+      .sort_values(ascending=False)
+)
+
+top_user = (
+    df.groupby("name")["amount"]
+      .sum()
+      .sort_values(ascending=False)
+      .index[0]
+)
+
+district_sum = (
+    df.groupby("district")["amount"]
+      .sum()
+      .sort_values(ascending=True)
+)
+
+district_sum.plot(kind="line")
+
+plt.title("Total Payments by District")
+plt.xlabel("District")
+plt.ylabel("Total Payments")
+
+plt.savefig("payments_by_district.png")
+plt.show()
+
+district_sum.to_csv("district_report.csv")
+district_sum.to_excel("district_report.xlsx")
